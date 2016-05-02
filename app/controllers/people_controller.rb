1ec0af165b339651c19752ae822bd5b00aac7bfb
rogerio <rogerio@rogerio>
2016-05-02 20:15:27 -0300

    Alterado Controllers people, friends e phones, alterado model person, alterado as views de people, phones, friends, e contacts

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,6 +10,7 @@ class PeopleController < ApplicationController
   # GET /people/1
   # GET /people/1.json
   def show
+    #@phone = @person.phones.find(params[:id])
   end
 
   # GET /people/new
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,8 @@
 class Person < ActiveRecord::Base
+  has_many :phones
+  has_many :friends
+
+
+  validates :status, presence: true
+
 end
