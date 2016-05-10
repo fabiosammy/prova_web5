7d9bb21bc7e9f0ad737ec88a73689e359ad7fbb3
andre.gomes <work.andregomes@gmail.com>
2016-05-09 21:51:50 -0300

    Testes

---
+++ test/controllers/phones_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class People::PhonesControllerTest < ActionController::TestCase
+  setup do
+    @person = people(:Person_1)
+    @phone = phones(:Phone_1)
+  end
+
+  test "should index phones" do
+    get :index, person_id: @person
+    assert_response :success
+  end
+
+  test "should get new phone" do
+    get :new, person_id: @person
+    assert_response :success
+  end
+
+  test "should create phone" do
+    assert_difference('Person.find(@person.id).phones.count') do
+      post :create, person_id: @person, phone: { number: "+55(042) 09988-1010", default: false }
+    end
+    assert_redirected_to person_phone_path(@person,assigns(:phone))
+  end
+
+  test "should show phone person" do
+    get :show, person_id: @person, id: @phone
+    assert_response :success
+    get :show, person_id: @person, id: @phone
+    assert_response :success, :format => :json
+  end
+
+  test "should destroy phone of person" do
+   assert_difference('Person.find(@person.id).phones.count', -1) do
+      delete :destroy, person_id: @person, id: @phone
+    end
+    assert_redirected_to person_phones_path(@person)
+  end
+end
